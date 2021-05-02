3cda0b3359a3b140341c0aef2ae26406cfa87f3e
Falcon <joonto7828@gmail.com>
2021-05-02 21:10:57 +0900

    forked by x4nt055

---
+++ test_ai.py
@@ -0,0 +1,12 @@
+from deep_emotion_recognition import DeepEmotionRecognizer
+# initialize instance
+# inherited from emotion_recognition.EmotionRecognizer
+# default parameters (LSTM: 128x2, Dense:128x2)
+deeprec = DeepEmotionRecognizer(emotions=['angry', 'sad', 'neutral', 'ps', 'happy'], n_rnn_layers=2, n_dense_layers=1, rnn_units=128, dense_units=128)
+# train the model
+deeprec.train()
+# get the accuracy
+print(deeprec.test_score())
+# predict angry audio sample
+prediction = deeprec.predict('data/validation/Actor_10/03-02-05-02-02-02-10_angry.wav')
+print(f"Prediction: {prediction}")
